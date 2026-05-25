c20267d1c4d55088a66e4afc46dfbf9ca6cb6299
kamaDe25 <kamalikade25@gmail.com>
2026-05-24 22:40:37 -0700

    main menu animations done

--- scenes/prototypes/Main-Menu.js
+++ scenes/prototypes/Main-Menu.js
@@ -13,9 +13,9 @@ export default class MainMenu extends Phaser.Scene {
     create() {
         this.add.image(640, 360, 'bg');
         this.add.image(640, 400, 'title');
-        const start = this.add.image(640, 400, 'start').setAlpha(0);
-        const settings = this.add.image(640, 500, 'settings').setAlpha(0);
-        const quit = this.add.image(640, 600, 'quit').setAlpha(0);
+        const start = this.add.image(640, 400, 'start').setAlpha(0).setInteractive();
+        const settings = this.add.image(640, 500, 'settings').setAlpha(0).setInteractive();
+        const quit = this.add.image(640, 600, 'quit').setAlpha(0).setInteractive();
 
         const fadeIn = (button, delayTime) =>{
             this.tweens.add({
@@ -30,5 +30,17 @@ export default class MainMenu extends Phaser.Scene {
         fadeIn(start, 0);
         fadeIn(settings, 2000);
         fadeIn(quit, 4000);
+
+        start.on('pointerdown', ()=> start.setTint(0x965A0B));
+        start.on('pointerup', ()=>start.clearTint());
+        start.on('pointerout', ()=>start.clearTint());
+
+        settings.on('pointerdown', ()=> settings.setTint(0x965A0B));
+        settings.on('pointerup', ()=>settings.clearTint());
+        settings.on('pointerout', ()=>settings.clearTint());
+
+        quit.on('pointerdown', ()=> quit.setTint(0x965A0B));
+        quit.on('pointerup', ()=>quit.clearTint());
+        quit.on('pointerout', ()=>quit.clearTint());
     }
 }

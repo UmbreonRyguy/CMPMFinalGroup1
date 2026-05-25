6c574f0d75d2df04925d72a681e35acaa383bdd0
kamaDe25 <kamalikade25@gmail.com>
2026-05-24 22:19:26 -0700

    buttons hardcoded to fade one at a time

--- scenes/prototypes/Main-Menu.js
+++ scenes/prototypes/Main-Menu.js
@@ -13,8 +13,22 @@ export default class MainMenu extends Phaser.Scene {
     create() {
         this.add.image(640, 360, 'bg');
         this.add.image(640, 400, 'title');
-        this.add.image(640, 400, 'start');
-        this.add.image(640, 500, 'settings');
-        this.add.image(640, 600, 'quit');
+        const start = this.add.image(640, 400, 'start').setAlpha(0);
+        const settings = this.add.image(640, 500, 'settings').setAlpha(0);
+        const quit = this.add.image(640, 600, 'quit').setAlpha(0);
+
+        const fadeIn = (button, delayTime) =>{
+            this.tweens.add({
+                targets: button,
+                alpha: 1,
+                ease: 'linear',
+                duration: 2000,
+                delay: delayTime
+            });
+        }
+        
+        fadeIn(start, 0);
+        fadeIn(settings, 2000);
+        fadeIn(quit, 4000);
     }
 }

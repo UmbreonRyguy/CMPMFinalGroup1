285bc7fd05710fa81b93f543e00cad3de1d5f67e
kamaDe25 <kamalikade25@gmail.com>
2026-05-26 15:29:24 -0700

    added new files

--- scenes/prototypes/cinematics.js
+++
@@ -1,9 +0,0 @@
-class CinematicsPrototype extends Phaser.Scene {
-    constructor() {
-        super('cinematics');
-    }
-
-    create() {
-        this.add.rectangle(200, 200, 100, 100, 0x00ffff);
-    }
-}

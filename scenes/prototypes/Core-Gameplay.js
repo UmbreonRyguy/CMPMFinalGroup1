7f5aa315d2931f19f2e385ac9cb58e197876a825
UmbreonRyguy <73410428+UmbreonRyguy@users.noreply.github.com>
2026-05-28 13:42:26 -0700

    Rearange UI in gameplay scene

--- scenes/prototypes/Core-Gameplay.js
+++ scenes/prototypes/Core-Gameplay.js
@@ -14,7 +14,7 @@ export default class GameplayPrototype extends Phaser.Scene {
         this.itemsHeld = 0;
         //this.add.rectangle(100, 100, 100, 100, 0x00ff00);
 
-        const itemText = this.add.text(100, 100, "item for player to pick up", {color: "#ffffff", backgroundColor: '#e03f3f', padding: { x: 60, y: 20 }}).setInteractive();
+        const itemText = this.add.text(1200, 200, "item for player to pick up", {color: "#ffffff", backgroundColor: '#e03f3f', padding: { x: 20, y: 10 }}).setInteractive();
         this.itemText = this.add.text(100, 180, "The player has " + this.itemsHeld + " items right now", {color: "#ffffff"});
         itemText.on('pointerup',()=>{
             itemText.destroy();
@@ -24,19 +24,19 @@ export default class GameplayPrototype extends Phaser.Scene {
             
         });
 
-        const returnButtonText = this.add.text(640, 650, "Return to Menu", {color: "#fffcfc", backgroundColor: '#3f1352', padding: { x: 60, y: 20 }}).setOrigin(0.5).setInteractive();
+        const returnButtonText = this.add.text(1200, 100, "Return to Menu", {color: "#fffcfc", backgroundColor: '#3f1352', padding: { x: 20, y: 10 }}).setOrigin(0.5).setInteractive();
         returnButtonText.on('pointerdown', ()=> returnButtonText.setTint(0x965A0B));
         returnButtonText.on('pointerup', ()=>{
             this.scene.start('level-select');
         });
 
-        const endSceneText = this.add.text(1000, 650, "Go to end scene", {color: "#ffffff", backgroundColor: '#3f1352', padding: { x: 60, y: 20 }}).setOrigin(0.5).setToTop().setInteractive();
+        const endSceneText = this.add.text(1200, 150, "Go to end scene", {color: "#ffffff", backgroundColor: '#3f1352', padding: { x: 20, y: 10 }}).setOrigin(0.5).setToTop().setInteractive();
         endSceneText.on('pointerdown', ()=> endSceneText.setTint(0x965A0B));
         endSceneText.on('pointerup', ()=>{
             this.scene.start('end-scene', { itemsHeld: this.itemsHeld });
         });
 
-        this.pauseButton = this.add.text(1000, 50, "Pause", {color: "#ffffff", backgroundColor: '#333333', padding: { x: 20, y: 10 }}).setOrigin(0.5).setSize(100, 100).setInteractive();
+        this.pauseButton = this.add.text(1200, 50, "Pause", {color: "#ffffff", backgroundColor: '#333333', padding: { x: 20, y: 10 }}).setOrigin(0.5).setSize(100, 100).setInteractive();
         //this.pauseButton.on('pointerover', () =>this.pauseButton.setTint(0xFF5C5));
         this.pauseButton.on('pointerup', ()=> {
             console.log("pause button clicked");

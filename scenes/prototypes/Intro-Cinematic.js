f1f6ec78b69c15f3dcdfe50e12e8570d7929dfc5
saosako-school <saosako@ucsc.edu>
2026-05-29 01:05:52 -0700

    added assets for switch, added switch that switches game between past and present (makes conveyor belt disappear, makes mushroom appear, changes how collision works for that platform) which only can be interacted with while the player is in range and which gets an outline when the player is in said range, made conveyor belt add negative velocity to player when standing on it, made mushroom make player bounce when on it (although this doesn't fully work as intended)

--- scenes/prototypes/Intro-Cinematic.js
+++ scenes/prototypes/Intro-Cinematic.js
@@ -94,6 +94,7 @@ export default class IntroCinematic extends Phaser.Scene {
         this.load.image('title', 'assets/finalproj-main-menu-prototype-titlesign.png');
         this.load.tilemapTiledJSON("prototypeTilemap", "assets/prototypeTilemap.json");
         this.load.spritesheet("Prototype_Tiles", "assets/Prototype_Tiles.png", {frameWidth: 80, frameHeight: 80});
+        this.load.atlas("levers", "assets/levers.png", "assets/textureAtlas.json");
         this.load.image('player', 'assets/PlaceholderPlayer.png');
         this.load.image('jumpButton', 'assets/LargerArrowButton.png');
         this.load.image('leftButton', 'assets/LargerArrowButton.png');

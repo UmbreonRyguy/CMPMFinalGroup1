c6124793f499c5ee0649e90690f59e45a4f4a5f3
Quetzal <quetzal.theobald@gmail.com>
2026-06-04 16:01:37 -0700

    test variable jump height change for mobile

--- scenes/prototypes/Core-Gameplay.js
+++ scenes/prototypes/Core-Gameplay.js
@@ -457,7 +457,7 @@ export default class GameplayPrototype extends Phaser.Scene {
 
         // variable jump height
         if ((this.cursors.up.isDown || this.touchJump) && this.player.body.velocity.y < -75) {
-            this.player.setVelocityY(this.player.body.velocity.y - 1.75);
+            this.player.setVelocityY(this.player.body.velocity.y - 5.75);
         }
 
         //let answer
